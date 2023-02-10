66bec328fe2e4d0c85559cb015af5b43c2f78462
Ubuntu <ubuntu@ip-172-31-42-37.ap-northeast-1.compute.internal>
2023-02-10 12:53:51 +0000

    first commit

---
+++ provider.tf
@@ -0,0 +1,10 @@
+#creating the aws instances
+resource "aws_instance" "jeevi"{
+ami = "ami-0cd7ad8676931d727"
+instance_type = "t3.micro"
+count = 2
+key_name = "new"
+tags = {
+Name = "jeevi"
+}
+}
